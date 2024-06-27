04faf58e7d2e54f3efa81950364eb7fa607668fd
Moocha <137570653+imMoocha@users.noreply.github.com>
2024-06-27 17:22:50 -0400

    Tarea de Sistema de Gestión de Datos

---
+++ GestionDeGranjaSlime/src/main/java/GestionDeGranjaSlime.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author misae
+ */
+public class GestionDeGranjaSlime {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
